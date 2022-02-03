8de4d646ce0e1559ef2ed1ad2d142b173c5c0725
nrsiward <nrsiward@users.noreply.github.com>
2022-02-02 20:12:55 -0600

    Added Macedonian variant for 0453 to locl and cv84 [RFS-805]. Updated ftml for latest changes. Improved ftml font labels. Added a utility script for updating feat_all.xml

--- tools/genftml.py
+++ tools/genftml.py
@@ -92,13 +92,16 @@ for test in test_lst:
     # generate multi-font test
     arg_lst = [arg.format(**arg_values_dict) for arg in arg_cols_template_lst]
     for fn in ttf_fn_sort_lst:
-        arg_lst.extend(["-s", "../results/{}".format(fn)])
-    # TODO: kludgy way to add columns for v5 and Doulos, assumes tests/reference folder
-    arg_lst.extend(["-s", "../references/v5/Andika-Regular.ttf=ARv5"])
-    # arg_lst.extend(["-s", "../references/v5/CharisSIL-Regular.ttf=CRv5"])
-    # arg_lst.extend(["-s", "../references/v5/CharisSIL-Italic.ttf=CIv5"])
-    # arg_lst.extend(["-s", "../references/b1/DoulosSIL-Regular.ttf=DRb1"])
-    # arg_lst.extend(["-s", "../references/v5/DoulosSIL-Regular.ttf=DRv5"])
+        family = fn[0]
+        family += 'Bk' if fn.find('Book') != -1 else ''
+        style = fn[fn.index('-') + 1]
+        style += 'I' if fn.find('BoldItalic') != -1 else ''
+        arg_lst.extend(["-s", "../results/{}={}{}".format(fn, family, style)])
+    # TODO: kludgy way to add columns for other fonts, assumes tests/reference folder
+    arg_lst.extend(["-s", "../references/v6001/Andika-Regular.ttf=AR6"])
+    arg_lst.extend(["-s", "../references/v6001/Andika-Bold.ttf=AB6"])
+    arg_lst.extend(["-s", "../references/v6001/Andika-Italic.ttf=AI6"])
+    arg_lst.extend(["-s", "../references/v6001/Andika-BoldItalic.ttf=ABI6"])
     sys.argv = [psfgenftml.__file__]
     sys.argv.extend(arg_lst)
     psfgenftml.cmd()
